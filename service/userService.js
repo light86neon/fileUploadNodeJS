889a6921e84d48106401a9412ebaf9436c6cafd6
light86neon <72137342+light86neon@users.noreply.github.com>
2021-03-19 10:19:47 +0200

    first commit

--- service/userService.js
+++ service/userService.js
@@ -2,7 +2,29 @@ const User = require('../dataBase/models/User');
 require('../dataBase/models/Car');
 
 module.exports = {
-    findUsers: (filterObject) => User.find(filterObject),
+    findUsers: (query = {}) => {
+        // limit , page використовуємо для пагінації;
+        const { limit = 20, page = 1, ...filters } = query;
+        // кількість сторінок яких потрібно пропустити
+        const skip = (page - 1) * limit;
+        const keys = Object.keys(filters);
+        const filterObject = {};
+
+        keys.forEach((key) => {
+            switch (key) {
+                case 'priceGte':
+                    filterObject.price = { $gte: filters.priceGte };
+                    break;
+                case 'priceLte':
+                    filterObject.price = { ...filterObject.price, $lte: +filters.priceLte };
+                    break;
+                default:
+                    filterObject[key] = filters[key];
+            }
+        });
+
+        User.find(filterObject).limit(limit).skip(skip);
+    },
 
     findUserById: (userId) => User.findById(userId),
 
